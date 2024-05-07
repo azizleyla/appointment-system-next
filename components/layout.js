4e74bb89abd806f289f916544fa51ee2bea18877
Leyla Aziz <ezizovaleyla1@gmail.com>
2024-05-08 00:14:42 +0400

    config context api for sidebar

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import Sidebar from "./sidebar";
 import { Main } from "next/document";
 import { Box, Grid } from "@mui/material";
 import { Button } from "@mui/base";
 import Topbar from "./topbar";
 
+export const SidebarContext = createContext();
+
 const Layout = ({ children }) => {
-    const [isOpenSidebar, setIsOpenSidebar] = React.useState(true);
+    const [isOpenSidebar, setIsOpenSidebar] = useState(true);
 
     const handleToggle = (open) => (event) => {
         if (
@@ -19,22 +21,20 @@ const Layout = ({ children }) => {
 
         setIsOpenSidebar(open);
     };
+
     return (
-        <Box sx={{ height: "100vh" }}>
-            <Sidebar
-                setIsOpenSidebar={setIsOpenSidebar}
-                isOpenSidebar={isOpenSidebar}
-                toggleDrawer={handleToggle}
-            />
-            <main className={`${isOpenSidebar ? "mainLeft" : ''}`}>
-                <Topbar
-                    toggleDrawer={handleToggle}
-                    setIsOpenSidebar={setIsOpenSidebar}
-                    isOpenSidebar={isOpenSidebar}
-                />
-                {children}
-            </main>
-        </Box>
+        <SidebarContext.Provider
+            value={{ isOpenSidebar, setIsOpenSidebar, handleToggle }}
+        >
+            <Box sx={{ height: "100vh" }}>
+                <Sidebar />
+                <main className={`${isOpenSidebar ? "mainLeft" : ""}`}>
+                    <Topbar />
+                    {children}
+                </main>
+            </Box>
+        </SidebarContext.Provider>
+
     );
 };
 
--- components/topbar/index.js
+++ components/topbar/index.js
@@ -1,13 +1,15 @@
 import { Box, Button } from "@mui/material";
-import React from "react";
+import React, { useContext } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
+import { SidebarContext } from "../layout";
 
-const Topbar = ({ toggleDrawer, isOpenSidebar }) => {
+const Topbar = () => {
+    let { isOpenSidebar, handleToggle } = useContext(SidebarContext);
+
     return (
         <Box
             sx={{
                 position: "fixed",
-
                 right: 0,
                 top: 0,
                 transition: "all 0.3s",
@@ -21,7 +23,7 @@ const Topbar = ({ toggleDrawer, isOpenSidebar }) => {
                 alignItems="center"
                 sx={{ position: "relative", padding: "0 24px", background: "#fff", height: "71px" }}
             >
-                <Button sx={{ padding: 0, minWidth: "36px", width: "36px", height: "36px", border: "1px solid #396cf01a", display: "flex", boxShadow: "0 3px 5px 0 #396cf04d", backgroundColor: "#396cf01a", borderRadius: "30px", alignItems: "center", justifyContent: "center" }} onClick={toggleDrawer(!isOpenSidebar)}>
+                <Button sx={{ padding: 0, minWidth: "36px", width: "36px", height: "36px", border: "1px solid #396cf01a", display: "flex", boxShadow: "0 3px 5px 0 #396cf04d", backgroundColor: "#396cf01a", borderRadius: "30px", alignItems: "center", justifyContent: "center" }} onClick={handleToggle(!isOpenSidebar)}>
                     <MenuIcon />
                 </Button>
             </Box>
